a58b2d9d1c66419aa85ada1dad3378c5afe521b8
AntonioGarciaRamirez <antgarramm@gmail.com>
2023-05-20 14:52:38 +0200

    Creacion y mod.
    Commit de creacion IDE CitaTe (Springboot) y modf. diagrama E/R FINAL

---
+++ Springboot/CitaTe/src/main/java/es/eoi/CitaTe/entities/Facturacion.java
@@ -0,0 +1,22 @@
+package es.eoi.CitaTe.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "facturacion")
+public class Facturacion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_facturacion", nullable = false)
+    private Long id;
+    private DateTimeFormat fecha;
+    private Pago pago;
+
+}
---
+++ Springboot/CitaTe/src/main/java/es/eoi/CitaTe/entities/Rol.java
@@ -0,0 +1,20 @@
+package es.eoi.CitaTe.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "rol")
+public class Rol {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_rol", nullable = false)
+    private Long id;
+    private String nombreRol;
+
+}
