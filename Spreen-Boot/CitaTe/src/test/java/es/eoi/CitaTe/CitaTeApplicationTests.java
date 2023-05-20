89e0b56d2453ea215d69984fa57c7639c761709a
AntonioGarciaRamirez <antgarramm@gmail.com>
2023-05-20 12:23:55 +0200

    Subo nuevo proyecto CitaTe en Springboot a GitH (añadiendo 9 files)

---
+++ Spreen-Boot/CitaTe/src/test/java/es/eoi/CitaTe/CitaTeApplicationTests.java
@@ -0,0 +1,13 @@
+package es.eoi.CitaTe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitaTeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
