8a0434e045c9f6b7eaeb05966519c5d9dcd4653e
AntonioGarciaRamirez <antgarramm@gmail.com>
2023-05-24 11:17:54 +0200

    Commit @TEMPORAL
    Hago commit de @TEMPORAL añadido en clase "Disponibilidad", en el campo "fecha".
    
    Se añaden archivos .DS del mac

--- Springboot/CitaTe/src/main/java/com/eoi/CitaTe/entities/Disponibilidad.java
+++ Springboot/CitaTe/src/main/java/com/eoi/CitaTe/entities/Disponibilidad.java
@@ -18,6 +18,7 @@ public class Disponibilidad {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id_disponibilidad", nullable = false)
     private Long id;
+    @Temporal(TemporalType.TIMESTAMP)
     private Date fecha;
     private Time hora_inicio;
     private Time hora_fin;
