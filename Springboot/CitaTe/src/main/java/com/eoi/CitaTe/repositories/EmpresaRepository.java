1516548714116b10828ebf8cce5ee04110536b66
SrFastGestor <hayamaquerica@gmail.com>
2023-06-12 03:17:21 +0200

    en pruebas, pendiente configurar controller bien, revisar el if no funciona bien PageImpl<>

--- Springboot/CitaTe/src/main/java/com/eoi/CitaTe/repositories/EmpresaRepository.java
+++ Springboot/CitaTe/src/main/java/com/eoi/CitaTe/repositories/EmpresaRepository.java
@@ -1,7 +1,11 @@
 package com.eoi.CitaTe.repositories;
 
 import com.eoi.CitaTe.entities.Empresa;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
+
+    Page<Empresa> findByDireccionProvinciaIgnoreCase(String provincia, Pageable pageable);
 }
